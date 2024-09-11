d494f79043dc48b93705b601859747a69f6a4b4b
AbdelruhmanSamy <abdelruhman.mohamad04@eng-st.cu.edu.eg>
2024-09-11 19:05:53 +0300

    1 problem added

---
+++ CF/Covered Path_534B.cpp
@@ -0,0 +1,68 @@
+    #include<bits/stdc++.h>
+
+    using namespace std;
+
+    typedef long long ll;
+    #define pii pair<int , int>
+    #define lp(i, j, n) for (int i = j; i <= n; i++)
+    #define vi vector<int>
+    #define popcnt(a) __builtin_popcount(a)
+    #define vvi vector<vector<int>>
+    #define vll vector<ll>
+    #define vch vector<char>
+    #define vvll vector<vector<ll>>
+    #define vvch vector<vector<char>>
+    #define vpi vector<pii>
+    #define veclp(n) for(auto it=n.begin() ; it!=n.end();it++)
+    #define mp make_pair
+    #define all(v) v.begin() , v. end()
+    #define MAX 1e4+5
+    #define rep(i, v) for(int i =0 ; i<v.size() ; i++)
+    #define scn(v) for (auto& i : v)cin >> i;
+    #define oo 1e18
+    #define cond(flag) if(flag)cout<<"YES";else cout<<"NO";
+
+    const ll MOD =  1e9;
+    const double PI = acos(-1.0);
+
+    void YallaSolve() {
+        ll st , end;
+        ll t, d;
+        cin>>st>>end>>t>>d;
+
+        ll ans = st+end;
+
+        ll pre = st;
+        lp(i , 2 , t-1) {
+            pre = min(end + (t - i) * d, pre+d);
+            ans+=pre;
+        }
+
+        cout<<ans;
+    }
+
+
+    void LotsOfTests() {
+        int t;
+        cin >> t;
+        lp(i , 1 , t) {
+            YallaSolve();
+            cout << "\n";
+        }
+    }
+    void PLAY() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+        cout << fixed << setprecision(10);
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+    }
+
+    int main() {
+        PLAY();
+    //        LotsOfTests();
+        YallaSolve();
+    }
